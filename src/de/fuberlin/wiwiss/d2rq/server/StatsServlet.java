657f142f2363a2c102554e26f79b24aafb17dc40
root <root@dbis-nowplaying.uibk.ac.at>
2014-05-07 15:22:59 +0200

    Read Statistics

--- src/de/fuberlin/wiwiss/d2rq/server/StatsServlet.java
+++ src/de/fuberlin/wiwiss/d2rq/server/StatsServlet.java
@@ -3,6 +3,9 @@ package de.fuberlin.wiwiss.d2rq.server;
 import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Date;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -27,6 +30,35 @@ public class StatsServlet extends HttpServlet {
 		VelocityWrapper velocity = new VelocityWrapper(this, request, response);
 		Context context = velocity.getContext();
 		context.put("classmap_links", classMapLinks);
+
+		try {
+    			BufferedReader br = new BufferedReader(new FileReader("stats.txt"));
+    		
+        		String line = br.readLine();
+
+			context.put("artists", line);
+        		line = br.readLine();
+	
+			context.put("tracks", line);
+			line = br.readLine();
+
+			context.put("users", line);
+			line = br.readLine();
+
+			context.put("listening_events", line);
+			line = br.readLine();
+
+			context.put("updated_at", line);
+
+			br.close();
+    		} catch(Exception e) {
+			context.put("artists", new Integer(0));
+			context.put("tracks", new Integer(0));
+			context.put("users", new Integer(0));
+			context.put("listening_events", new Integer(0));
+			context.put("updated_at", "...");
+		}	
+
 		velocity.mergeTemplateXHTML("stats_page.vm");
 	}
 
