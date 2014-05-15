c5a5afeed254cd6e80e5b94de374ec054568b95e
root <root@dbis-nowplaying.uibk.ac.at>
2014-05-15 17:07:54 +0200

    Setup repo for displaying no passwords

--- scripts/dumps.sh
+++ scripts/dumps.sh
@@ -2,7 +2,7 @@
 
 echo 'Start updating the dumps..'
 
-./dump-rdf -f N-TRIPLE /home/user/DBIS/webapp/WEB-INF/mapping.ttl > /home/jetty/nowplaying-dump.nt
-./dump-rdf -f RDF/XML /home/user/DBIS/webapp/WEB-INF/mapping.ttl > /home/jetty/nowplaying-dump.rdf
+./dump-rdf -f N-TRIPLE /home/user/DBIS/scripts/mapping.ttl > /home/jetty/nowplaying-dump.nt
+./dump-rdf -f RDF/XML /home/user/DBIS/scripts/mapping.ttl > /home/jetty/nowplaying-dump.rdf
 
 echo 'Finished updating the dumps..'
